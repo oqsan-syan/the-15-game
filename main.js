56657f6b092340805bfb6b1e7fdc363bc778f3b5
oqsan-syan <osurenyan@bk.ru>
2020-08-23 17:19:22 +0400

    Update main.js

--- main.js
+++ main.js
@@ -70,16 +70,6 @@ arr.forEach((item, index, array) => {
 });
 
 
-    //  let alertBox = document.createElement('div');
-    //  let btn = document.createElement('button');
-    //  alertBox.classList.add('alert');
-    //  btn.classList.add('btn');
-    //  header.appendChild(alertBox);
-    //  header.appendChild(btn);
-    //  alertBox.textContent = "Congratulations, You Won!!!";
-    //  btn.textContent = 'start again';
-    //  btn.addEventListener('click', () => {window.location.reload()} );
-
 
 
 
